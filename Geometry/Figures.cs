a28fcee1b991ea82ac0db0e92a81fcb1920e89ec
KuebV <rwthompson01@bvsd.org>
2021-09-13 11:18:08 -0600

    Initial Commit

--- Geometry/Figures.cs
+++ Geometry/Figures.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MathPlus
+namespace MathPlus.Geometry
 {
-    public class Class1
+    class Figure
     {
     }
 }
